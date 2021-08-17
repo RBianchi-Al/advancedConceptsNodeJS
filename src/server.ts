5ab32050a996c3a59dada5c30a44981a1819a578
RBianchi-Al <renatalebianchi@gmail.com>
2021-08-17 13:08:09 -0300

    Post e Get - Category

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express from "express";
 
-const app = express();
+import { categoriesRoutes } from "./routes/categories.routes";
 
-app.get("/", (request, response) => {
-    response.json({ message: "Wello Igniteadvanced concepts Node" });
-});
+const app = express();
+app.use(express.json());
+
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => console.log("Server is running 🚀"));
